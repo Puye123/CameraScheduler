eeb5b5b5827d161235f000df6b344393bfd8f90b
a2c <a2cgle@gmail.com>
2018-06-28 19:53:41 +0900

    シリアルの受信部分のサンプル
    電文はb'\r' が付与されたり
    0.05秒間隔のキャプチャーで分断されたり
    することを考慮して、特定のコマンドで反応出来るようにした。

---
+++ src/cap_serial_sample.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+
+def main():
+
+  with serial.Serial('/dev/tty.usbserial-FT2J6I04',38400,timeout=0.05) as ser:
+    _evt = {
+        'original':{  # 制御装置から送られてくる電文
+          b'R00\r':"stop",
+          b'R01\r':"normal start",
+          b'R02\r':"event no 2",
+          b'R03\r':"event no 3",
+          b'R09\r':"Black screen",
+          },
+        'judge':{  # 動作判定用の文言
+          b'R00':"stop",
+          b'R01':"normal start",
+          b'R02':"event no 2",
+          b'R03':"event no 3",
+          b'R09':"Black screen",
+          }
+        }
+
+    cur_cmd  = b''
+
+    while True:
+      line = ser.readline()
+      cur_cmd += line
+
+      if b'\r' in cur_cmd:
+        go_cmd = cur_cmd.split('\r')[0]
+        cur_cmd = cur_cmd.split('\r')[1]
+      else:
+        continue
+
+      if go_cmd in _evt['judge']:
+        print( _evt['judge' ][ go_cmd ] )
+        # do something
+      else:
+        print( 'unknown command' )
+
+if __name__ == "__main__":
+    main()
+
